ac6af6b5b1f597f6893845242ddcd31fd2caab1a
centinela1024tb <centinela1024tb@gmail.com>
2023-01-10 15:57:27 -0600

    [ADD] Se configura react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,9 +12,10 @@ import './assets/styles.css';
 const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <RouterProvider router={ router } />
-    </QueryClientProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={client}>
+			<ReactQueryDevtools />
+			<RouterProvider router={ router } />			
+		</QueryClientProvider>
+	</React.StrictMode>
 )
